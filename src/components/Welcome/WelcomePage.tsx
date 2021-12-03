f6fc4a87ecfe0c237b02b44d40c6a27290351cdd
Bob Fisher <bobfisher@pop-os.localdomain>
2021-12-03 15:43:57 -0600

    Updated Welcome page and installed material-ui styles

--- src/components/Welcome/WelcomePage.tsx
+++ src/components/Welcome/WelcomePage.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import Button from '@mui/material/Button';
+import { fabClasses, Typography } from '@mui/material';
+import { createStyles, makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material/styles';
+
 import OpenLink from '../Practice/OpenLink';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      textAlign: 'center'
+    },
+    title: {
+      backgroundColor: 'red',
+    },
+  }),
+);
+
+
 const WelcomePage = () => {
+  const classes = useStyles();
   return (
-    <>
-      <h1>The Haunted Mall: Video Game</h1>
+    <div className={classes.root}>
+      <Typography
+        variant="h1"
+        className={classes.title}
+      >
+        The Haunted Mall: Video Game
+      </Typography>
       <OpenLink />
-    </>
+    </div>
   )
 };
 
